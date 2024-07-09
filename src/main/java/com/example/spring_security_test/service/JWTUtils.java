bf10e459c9335f293e752d23d218e70024e0ea30
DoTheAnhh <dotheanh0112@gmail.com>
2024-07-09 09:03:50 +0700

    truyen role vao token

--- src/main/java/com/example/spring_security_test/service/JWTUtils.java
+++ src/main/java/com/example/spring_security_test/service/JWTUtils.java
@@ -21,7 +21,7 @@ public class JWTUtils {
     private SecretKey Key;
 
     private static final long EXPIRATION_TIME = 86400000; //24h
-    
+
     public JWTUtils() {
         String secretString = "843567893696976453275974432697R634976R738467TR678T34865R6834R8763T478378637664538745673865783678548735687R3";
         byte[] keyBytes = Base64.getDecoder().decode(secretString.getBytes(StandardCharsets.UTF_8));
